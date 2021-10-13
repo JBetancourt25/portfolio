84f610f01dac024651782c96a8dbf827c45db398
Jose Betancourt <josebp_25@hotmail.com>
2021-10-13 03:46:03 -0400

    third

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -16,15 +16,7 @@ export const projects = [
     visit: 'https://github.com/JBetancourt25/Responsive-Grid',
     id: 1,
   },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: '/images/3.jpg',
-    tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
+
   // {
   //   title: 'Unichat',
   //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
